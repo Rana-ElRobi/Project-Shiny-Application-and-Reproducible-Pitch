ed691c218bce578a9ab937299a09db2ea0742024
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2018-02-15 13:01:17 +0200

    created ui.R interface for app

--- FinalshinyApp/ui.R
+++ FinalshinyApp/ui.R
@@ -13,21 +13,44 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel(" Car Finder App "),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
+       checkboxGroupInput('am', 
+                          'Car Type', 
+                          c("Automatic"= 0, "Manual"= 1), 
+                          selected = 0 ),
+       checkboxGroupInput('cyl',
+                          'Select cylinders number',
+                          c("Four"=4, "Six"=6, "Eight"=8),
+                          selected = c(6,8)), 
+       sliderInput('disp', 
+                   'Displacement', 
+                   min=70, 
+                   max=480, 
+                   value=c(50,300),
+                   step=5),
+       numericInput('cost',
+                    'Range for Gasoline Price (per gallon)',
+                    min = 2,
+                    max = 5,
+                    step=0.01,
+                    value = 3.2),
+       numericInput('dis',
+                    'Distance Range (in miles)',
+                    min = 1,
+                    max = 500,
+                    value = 30)
+       #numericInput('gas', 'Maximum expenditure on gasoline:', 60, min=1, max=1000), 
+
+       #sliderInput('hp', 'Gross horsepower', min=50, max=340, value=c(50,340), step=10), 
+       ),
     
     # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+    mainPanel( 
+      dataTableOutput('table') 
+    ) 
   )
 ))
