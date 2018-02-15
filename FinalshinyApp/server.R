41fd4a7f4a8bcaa2cddcd86fe476387d3c70ae67
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2018-02-15 13:16:20 +0200

    applied the application code for displaying filtered data

--- FinalshinyApp/server.R
+++ FinalshinyApp/server.R
@@ -8,19 +8,44 @@
 #
 
 library(shiny)
+library(dplyr)
+library(datasets)
 
-# Define server logic required to draw a histogram
+# Define server filter data with input requirments
 shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
+  output$table <- renderDataTable({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # generat sequance data in range between min and max
+    disp_seq <- seq(from = input$disp[1], to = input$disp[2], by = 0.1) 
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # Re-shape the data
+    reshaped.data <- transmute(mtcars,
+                      Car = rownames(mtcars),
+                      MilesPerGallon = mpg,  
+                      GasolineExpenditure = input$dis/mpg*input$cost, 
+                      Cylinders = cyl, 
+                      Displacement = disp,  
+                      Transmission = am) 
+    # Filter target data
+    filtered.data <- filter(reshaped.data ,
+                   Cylinders %in% input$cyl,
+                   Displacement %in% disp_seq, 
+                   Transmission %in% input$am) 
     
-  })
+    # change transmission to labels 0/1
+    labelled.data <- mutate(filtered.data, 
+                   Transmission = ifelse(Transmission==0,
+                                         "Automatic",
+                                         "Manual"))
+    
+    # Sort/re-arrange data by GasolineExpenditure 
+    resultant.data <- arrange(labelled.data, GasolineExpenditure) 
+    # return final resultant data
+    resultant.data 
+    
+  },
+  # customize page lenght
+  options = list(lengthMenu = c(5, 15, 30), pageLength = 30) )
   
 })
